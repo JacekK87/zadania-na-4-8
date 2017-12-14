75b36969314785cd96d2d8af409b6d3417406136
JacekK87 <jacekielar1@wp.pl>
2017-12-14 23:24:38 +0100

    fibonaci

--- fib.c
+++ fib.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 
+int n, fib, t;
+int wybor = 1;
+
+void liczenie(int *n, int fib);
+
 int main() {
-    int n;
-    int fn,f1=n-2, f2=n-1;
-    int wynik;
 
+    do{
 
-    printf("Podaj wartosc calkowita dla ktorej mam obliczyc ciag \nn = ");
-    scanf(" %d", &n);
+        liczenie(&n, fib);
 
-    if (n <= 2) {
-        printf("Wynik = 1\n");
-    } else {
-        int fn= (f1 + f2);
-
-        printf("%d elementem ciagu Fibonacciego jest %d\n",n,fn);
+            printf("Czy chcesz liczyc kolejna liczbe?? 1 - Tak / 0 - Nie\n");
+            scanf("%d", &wybor);
     }
-    return fn = f1 + f2;
+    while(wybor == 1);
+
+    return 0;
+}
+
+    void liczenie(int *n, int fib) {
+    printf("Podaj wartosc calkowita dla ktorej mam obliczyc ciag \nn = ");
+    scanf(" %d", n);
+
+    if ((*n) <= 2) {
+            printf("Wynik = 1\n");
+        } else if ((*n>2)) {
+        fib = (*n) - 2 + (*n) - 1;
+        printf("Ciag Fibonacciego dla liczby %d jest liczba %d\n", (*n), fib);
+    }
 }
